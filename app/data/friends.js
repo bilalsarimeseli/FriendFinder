35f8979b72e5ffa8aa93abf0e0cab58d3568e9ce
Bilal Sarımeşeli <bilalsarimeseli@Bilals-Air.attlocal.net>
2019-09-29 21:45:42 -0700

    Initial files have been created

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+//The array that holds all the 'friends' that have taken the survey
+
+var friendList = [
+    {
+      name: 'Bilal Sarimeseli',
+      profilePic: 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwir8Ymp2vfkAhUDDKwKHV2eAv0QjRx6BAgBEAQ&url=https%3A%2F%2Ftwitter.com%2Fbilalsarimeseli&psig=AOvVaw3fXnVDmQlW2SjIGH38z1a1&ust=1569904010655695',
+      scores: [
+        "4",
+        "5",
+        "5",
+        "2",
+        "4",
+        "5",
+        "4",
+        "4",
+        "3",
+        "4"
+      ]
+    }
+  ]
+  
+  module.exports = friendList;
